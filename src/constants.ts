c067c6b46cf4a6fc85882c8d90cd230f6c4ccd74
rbhgn <robhgn@gmail.com>
2018-09-11 22:37:43 +0200

    Changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-//  export const DATABASE_URL = "postgres://postgres:secret@localhost:5432/postgres"
+ export const DATABASE_URL = "postgres://postgres:secret@localhost:5432/postgres"
 
- export const DATABASE_URL = "postgres://lilbfwgmumvudv:3c13d5e08b420847d3f49c59c0fe883e4272bd36218232977b7a7a7d4bf43317@ec2-54-217-236-201.eu-west-1.compute.amazonaws.com:5432/ddmnc53ck0q6nk"
+//  export const DATABASE_URL = "postgres://lilbfwgmumvudv:3c13d5e08b420847d3f49c59c0fe883e4272bd36218232977b7a7a7d4bf43317@ec2-54-217-236-201.eu-west-1.compute.amazonaws.com:5432/ddmnc53ck0q6nk"
